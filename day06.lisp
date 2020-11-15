0fc5e36c88210af3010f9ad66f1dc11725ef835a
Franck YVONNET <franck@yvonnet.fr>
2020-11-15 21:15:00 +0100

    use local variables for graphs

--- day06.lisp
+++ day06.lisp
@@ -11,10 +11,7 @@
 (in-package :day06)
 
 
-(defparameter *graph* (empty-map))
-
-
-(defun explore-graph (init-name init-depth test-name set-solution)
+(defun explore-graph (graph init-name init-depth test-name set-solution)
   (loop
     with queue = (make-queue :simple-queue)
     with solution = 0
@@ -28,30 +25,28 @@
     do (progn
          (setf visited (with visited name))
          (setf solution (funcall set-solution solution depth))
-         (dolist (n (lookup *graph* name))
+         (dolist (n (lookup graph name))
            (unless (contains? visited n)
              (qpush queue n)
              (qpush queue (1+ depth)))))
     finally (format t "~a~%" solution)))
 
-(defun populate-graph (input)
-  (setf
-    *graph*
-    (reduce
-      (lambda (g i) (with g (first i) (cons (second i) (lookup g (first i)))))
-      input
-      :initial-value *graph*)))
+(defun populate-graph (graph input)
+  (reduce
+    (lambda (g i) (with g (first i) (cons (second i) (lookup g (first i)))))
+    input
+    :initial-value graph))
 
 (defun main ()
-  (let
+  (let*
     ((input
        (with-open-file (stream "inputs/day06")
          (loop
            for line = (read-line stream nil)
            while line
-           collect (split "\\)" line)))))
+           collect (split "\\)" line))))
+     (directed-graph   (populate-graph (empty-map) input))
+     (undirected-graph (populate-graph directed-graph (mapcar #'reverse input))))
 
-    (populate-graph input)
-    (explore-graph "COM"  0 (lambda (n) (null n)) #'+)
-    (populate-graph (mapcar #'reverse input))
-    (explore-graph "YOU" -1 (lambda (n) (string= n "SAN")) (lambda (_ d) d))))
+    (explore-graph directed-graph   "COM"  0 (lambda (n) (null n)) #'+)
+    (explore-graph undirected-graph "YOU" -1 (lambda (n) (string= n "SAN")) (lambda (_ d) d))))
